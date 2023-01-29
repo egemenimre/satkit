da92cd80e2007c2ebb2a64836d5e1143375ab45c
egemenimre <s.egemen.imre@gmail.com>
2023-01-29 23:50:15 +0100

    added __sub_ and __add__ and tests

--- satkit/time/tests/test_time.py
+++ satkit/time/tests/test_time.py
@@ -27,3 +27,26 @@ def test_time_sort():
     assert str(sorted_list[0].getDate()) == "2020-07-10T23:56:00.000Z"
     assert str(sorted_list[1].getDate()) == "2020-07-11T00:00:00.000Z"
     assert str(sorted_list[-1].getDate()) == "2020-07-11T00:04:00.000Z"
+
+
+def test_time_add_sub():
+    """Tests the `AbsoluteDateExt` time addition and subtraction operands."""
+    date1 = AbsoluteDateExt(2020, 7, 11, 00, 0, 0.0, TimeScalesFactory.getUTC())
+    dt2 = -240.0  # normal float
+    dt3 = +4 * u.min  # Quantity
+    dt4 = +120  # int
+    date2_add = date1 + dt2
+    date3_add = date1 + dt3
+    date4_add = date1 + dt4
+
+    dt2_sub = date2_add - date1
+    dt3_sub = date3_add - date1
+    dt4_sub = date4_add - date1
+
+    assert str(date2_add.getDate()) == "2020-07-10T23:56:00.000Z"
+    assert str(date3_add.getDate()) == "2020-07-11T00:04:00.000Z"
+    assert str(date4_add.getDate()) == "2020-07-11T00:02:00.000Z"
+
+    assert dt2_sub == -240.0 * u.second
+    assert dt3_sub == 240.0 * u.second
+    assert dt4_sub == 120.0 * u.second
