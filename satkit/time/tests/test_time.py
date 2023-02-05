4c8e4566a54df35a91dba9bcd1f0c61f9f8dde34
egemenimre <s.egemen.imre@gmail.com>
2023-02-05 18:31:25 +0100

    added tests to datetime support and __sub__ extension

--- satkit/time/tests/test_time.py
+++ satkit/time/tests/test_time.py
@@ -7,7 +7,7 @@
 Time functions tests.
 
 """
-
+from orekit.pyhelpers import absolutedate_to_datetime
 from org.orekit.time import AbsoluteDate, TimeScalesFactory
 
 from satkit import u
@@ -29,23 +29,35 @@ def test_time_sort():
     assert str(sorted_list[-1].getDate()) == "2020-07-11T00:04:00.000Z"
 
 
-def test_time_add_sub():
+def test_time_add_sub_duration_from():
     """Tests the `AbsoluteDateExt` time addition and subtraction operands."""
     date1 = AbsoluteDateExt(2020, 7, 11, 00, 0, 0.0, TimeScalesFactory.getUTC())
     dt2 = -240.0  # normal float
     dt3 = +4 * u.min  # Quantity
     dt4 = +120  # int
+
     date2_add = date1 + dt2
     date3_add = date1 + dt3
     date4_add = date1 + dt4
+
+    date2_sub = date1 - dt2
+
+    assert str(date2_add.getDate()) == "2020-07-10T23:56:00.000Z"
+    assert str(date3_add.getDate()) == "2020-07-11T00:04:00.000Z"
+    assert str(date4_add.getDate()) == "2020-07-11T00:02:00.000Z"
+    assert str(date2_sub.getDate()) == "2020-07-11T00:04:00.000Z"
 
     dt2_sub = date2_add - date1
     dt3_sub = date3_add - date1
     dt4_sub = date4_add - date1
 
-    assert str(date2_add.getDate()) == "2020-07-10T23:56:00.000Z"
-    assert str(date3_add.getDate()) == "2020-07-11T00:04:00.000Z"
-    assert str(date4_add.getDate()) == "2020-07-11T00:02:00.000Z"
+    assert dt2_sub == -240.0 * u.second
+    assert dt3_sub == 240.0 * u.second
+    assert dt4_sub == 120.0 * u.second
+
+    dt2_sub = date2_add.durationFrom(date1)
+    dt3_sub = date3_add.durationFrom(date1)
+    dt4_sub = date4_add.durationFrom(date1)
 
     assert dt2_sub == -240.0 * u.second
     assert dt3_sub == 240.0 * u.second
@@ -53,11 +65,15 @@ def test_time_add_sub():
 
 
 def test_init():
-    """Tests the `AbsoluteDate` init."""
+    """Tests the `AbsoluteDate` and `datetime` init."""
     date1 = AbsoluteDate(2020, 7, 11, 00, 0, 0.0, TimeScalesFactory.getUTC())
     date2 = AbsoluteDateExt(date1)
+
+    date3 = AbsoluteDateExt(absolutedate_to_datetime(date1))
 
     # print(type(date1))
     # print(type(date2))
 
     assert isinstance(date2, AbsoluteDateExt)
+    assert date1.isEqualTo(date2)
+    assert date3.isEqualTo(date2)
