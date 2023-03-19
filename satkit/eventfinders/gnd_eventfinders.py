b54cf6a86402487f76636dca0bba661b4e205e99
egemenimre <s.egemen.imre@gmail.com>
2023-03-19 22:37:17 +0100

    added `gnd_illum_finder`, overhauled file

--- satkit/eventfinders/gnd_eventfinders.py
+++ satkit/eventfinders/gnd_eventfinders.py
@@ -6,25 +6,39 @@
 """
 Event finders for ground based events and intervals.
 """
-from org.orekit.bodies import GeodeticPoint, OneAxisEllipsoid
-from org.orekit.frames import TopocentricFrame
+from enum import Enum
+
+from org.orekit.bodies import CelestialBodyFactory, GeodeticPoint, OneAxisEllipsoid
+from org.orekit.frames import FramesFactory, TopocentricFrame
+from org.orekit.models import AtmosphericRefractionModel
 from org.orekit.propagation import Propagator
-from org.orekit.propagation.events import ElevationDetector, EventsLogger
+from org.orekit.propagation.events import (
+    ElevationDetector,
+    EventsLogger,
+    GroundAtNightDetector,
+)
 from org.orekit.propagation.events.handlers import ContinueOnEvent
-from org.orekit.utils import ElevationMask
+from org.orekit.utils import (
+    Constants,
+    ElevationMask,
+    IERSConventions,
+    PVCoordinatesProvider,
+)
 from pint import Quantity
 
 from satkit import u
+from satkit.propagation.orbits import generate_ephemeris_prop
 from satkit.time.timeinterval import TimeInterval, TimeIntervalList
 
 
-@u.wraps(None, (None, None, None, "rad", None), False)
+@u.wraps(None, (None, None, None, "rad", None, None), False)
 def gnd_pass_finder(
     search_interval: TimeInterval,
     propagator: Propagator,
-    gnd_pos: GeodeticPoint,
+    gnd_pos: GeodeticPoint | TopocentricFrame,
     elev_mask: float | Quantity | ElevationMask,
-    planet: OneAxisEllipsoid,
+    planet: OneAxisEllipsoid = None,
+    refraction_model: AtmosphericRefractionModel = None,
 ) -> TimeIntervalList:
     """
     Finds satellite (or any object with a trajectory) "passes" over a ground location.
@@ -45,45 +59,186 @@ def gnd_pass_finder(
                                  Constants.WGS84_EARTH_FLATTENING,
                                  itrf)
 
+    If the `gnd_pos` parameter is defined as a `TopocentricFrame`, then the optional planet parameter
+    is ignored. Otherwise, it is set to Earth as given above.
+
+    Atmospheric Refraction Model should be set to `None` for communications applications. It can be set to
+    `EarthITU453AtmosphereRefraction` or `EarthStandardAtmosphereRefraction` (provided by Orekit) for visual
+    or optical applications.
+
     Parameters
     ----------
     search_interval
-        search interval for the "elevation events"
+        Search interval for the "elevation events"
     propagator
-        propagator to generate the trajectory of the satellite (or any other object)
+        Propagator to generate the trajectory of the satellite (or any other object)
     gnd_pos
-        ground position in geodetic coordinates
+        Ground position in geodetic coordinates (or the topocentric frame associated with it)
     elev_mask
         Elevation mask, either a fixed value or a complex mask shape
     planet
-        The planet where the ground location is located.
+        The planet where the ground location is located. Defaults to WGS84 Earth.
+    refraction_model
+        Atmospheric Refraction Model, defaults to `None`
 
     Returns
     -------
+    TimeIntervalList
+        List of time intervals corresponding to the "elevation above the mask"
 
     """
 
     # init topocentric frame
-    topo_frame = TopocentricFrame(planet, gnd_pos, "ground pos")
+    topo_frame = _init_topo_frame(gnd_pos, planet)
 
-    # event detector: Elevation Mask
+    # Init event detector: Elevation Mask
     if isinstance(elev_mask, ElevationMask):
         # Use an elevation mask that is a function of azimuth angle
-        elev_detector = ElevationDetector(topo_frame).withElevationMask(elev_mask)
+        event_detector = ElevationDetector(topo_frame).withElevationMask(elev_mask)
     else:
         # Use a constant elevation mask
-        elev_detector = ElevationDetector(topo_frame).withConstantElevation(elev_mask)
+        event_detector = ElevationDetector(topo_frame).withConstantElevation(elev_mask)
 
-    elev_detector = elev_detector.withHandler(ContinueOnEvent())
+    # add atmospheric refraction model (or None)
+    event_detector = event_detector.withRefraction(refraction_model)
 
+    # do not stop at "rise" or "set" events (returns AbstractDetector)
+    event_detector = event_detector.withHandler(ContinueOnEvent())
+
+    # return the generated time interval list (g positive marks an interval)
+    return _find_g_pos_intervals(search_interval, propagator, event_detector)
+
+
+class StandardDawnDuskElevs(Enum):
+    """Standard elevations definitions."""
+
+    # Sun elevation at civil dawn/dusk (6° below horizon).
+    CIVIL_DAWN_DUSK_ELEVATION = -6.0 * u.deg
+
+    # Sun elevation at nautical dawn/dusk (12° below horizon).
+    NAUTICAL_DAWN_DUSK_ELEVATION = -12.0 * u.deg
+
+    # Sun elevation at astronomical dawn/dusk (18° below horizon).
+    ASTRONOMICAL_DAWN_DUSK_ELEVATION = -18.0 * u.deg
+
+
+@u.wraps(None, (None, None, "rad", None, None, None), False)
+def gnd_illum_finder(
+    search_interval: TimeInterval,
+    gnd_pos: GeodeticPoint | TopocentricFrame,
+    dawn_dusk_elev: float | Quantity,
+    planet: OneAxisEllipsoid = None,
+    refraction_model: AtmosphericRefractionModel = None,
+    sun_coords: PVCoordinatesProvider = None,
+) -> TimeIntervalList:
+    """
+    Finds satellite (or any object with a trajectory) "passes" over a ground location.
+
+    This method is not limited to a ground location on Earth (as defined by the `planet` parameter).
+    It uses the Orekit `ElevationDetector` to find the "elevation equal to elevation mask" events.
+    However, the cases with "no events in the search interval" are handled correctly. The output is
+    a `TimeIntervalList` which can then be intersected with another interval list, for example
+    "ground location illuminated intervals".
+
+    The method accepts both a fixed elevation mask or an `ElevationMask` with a complex mask shape.
+    It is also possible to use the values in the `StandardDawnDuskElevs` enumerator.
+
+    The planet parameter can be any `OneAxisEllipsoid` with its own fixed frame. For example,
+    Earth can be generated as follows:
+
+        itrf = FramesFactory.getITRF(IERSConventions.IERS_2010, True)
+        earth = OneAxisEllipsoid(Constants.WGS84_EARTH_EQUATORIAL_RADIUS,
+                                 Constants.WGS84_EARTH_FLATTENING,
+                                 itrf)
+
+    If the `gnd_pos` parameter is defined as a `TopocentricFrame`, then the optional planet parameter
+    is ignored. Otherwise, it is set to Earth as given above.
+
+    Atmospheric Refraction Model should be set to `None` for communications applications. It can be set to
+    `EarthITU453AtmosphereRefraction` or `EarthStandardAtmosphereRefraction` (provided by Orekit) for visual
+    or optical applications.
+
+    Parameters
+    ----------
+    search_interval
+        Search interval for the "events"
+    gnd_pos
+        Ground position in geodetic coordinates (or the topocentric frame associated with it)
+    dawn_dusk_elev
+        Elevation mask a fixed value
+    planet
+        The planet where the ground position is located. Defaults to WGS84 Earth.
+    refraction_model
+        Atmospheric Refraction Model, defaults to `None`
+    sun_coords
+        Propagator (or `PVCoordinatesProvider`) to generate the trajectory of the Sun
+
+    Returns
+    -------
+    TimeIntervalList
+        List of time intervals corresponding to the "elevation above the mask"
+    """
+
+    # Init topocentric frame
+    topo_frame = _init_topo_frame(gnd_pos, planet)
+
+    # generate Sun as a PVCoordinatesProvider
+    if not sun_coords:
+        sun_coords = PVCoordinatesProvider.cast_(CelestialBodyFactory.getSun())
+
+    # Init event detector: Ground at Night
+    event_detector = GroundAtNightDetector(
+        topo_frame, sun_coords, dawn_dusk_elev, refraction_model
+    )
+
+    # Generate an Ephemeris Propagator to hold the trajectory of the Sun
+    stepsize = 10 * 60 * u.sec
+    interpolation_points = 5
+    propagator = generate_ephemeris_prop(
+        search_interval,
+        sun_coords,
+        stepsize=stepsize,
+        frame=FramesFactory.getGCRF(),
+        interpolation_points=interpolation_points,
+    )
+
+    # return the generated time interval list (g negative marks an interval)
+    return _find_g_neg_intervals(search_interval, propagator, event_detector)
+
+
+def _find_g_pos_intervals(
+    search_interval: TimeInterval, propagator: Propagator, event_detector
+) -> TimeIntervalList:
+    """
+    Finds the intervals where the `g` function is positive. Increasing `g` event
+    marks the start of the interval and decreasing `g` event marks the end of
+    the interval.
+
+    Parameters
+    ----------
+    search_interval
+        Search interval for the "events"
+    propagator
+        Propagator to generate the trajectory of the satellite (or any other object)
+    event_detector
+        Event detector
+
+    Returns
+    -------
+    TimeIntervalList
+        Time interval list conforming to the events
+
+    """
     # add the event detector to the propagator
     logger = EventsLogger()
-    propagator.addEventDetector(logger.monitorDetector(elev_detector))
+    propagator.addEventDetector(logger.monitorDetector(event_detector))
 
     # Propagate from the initial date to the final date, logging increasing and decreasing events
     prop_end_state = propagator.propagate(search_interval.start, search_interval.end)
 
     # Categorise the events
+
+    # for g()>0, event is in interval
     start_state = None
     intervals = []
 
@@ -91,7 +246,7 @@ def gnd_pass_finder(
 
     if not events:
         # event list is empty, this means either the complete duration is valid or the complete duration is invalid
-        if elev_detector.g(prop_end_state) > 0:
+        if event_detector.g(prop_end_state) > 0:
             # the complete duration is an event
             intervals = [TimeInterval.from_interval(search_interval)]
         return TimeIntervalList(intervals, search_interval)
@@ -128,3 +283,72 @@ def gnd_pass_finder(
 
     # return the generated time interval list
     return TimeIntervalList(intervals, search_interval)
+
+
+def _find_g_neg_intervals(
+    search_interval: TimeInterval, propagator: Propagator, event_detector
+) -> TimeIntervalList:
+    """
+    Finds the intervals where the g function is negative. Increasing `g` event
+    marks the end of the interval and decreasing `g` event marks the start of
+    the interval.
+
+    Parameters
+    ----------
+    search_interval
+        Search interval for the "events"
+    propagator
+        Propagator to generate the trajectory of the satellite (or any other object)
+    event_detector
+        Event detector
+
+    Returns
+    -------
+    TimeIntervalList
+        Time interval list conforming to the events
+
+    """
+    return _find_g_pos_intervals(search_interval, propagator, event_detector).invert()
+
+
+def _init_topo_frame(
+    gnd_pos: GeodeticPoint | TopocentricFrame,
+    planet: OneAxisEllipsoid = None,
+) -> TopocentricFrame:
+    """
+    Initialises the Topocentric Frame.
+
+    Parameters
+    ----------
+    gnd_pos
+        Ground position in geodetic coordinates (or the topocentric frame associated with it)
+    planet
+        The planet where the ground position is located. Defaults to WGS84 Earth.
+
+    Returns
+    -------
+    TopocentricFrame
+        Topocentric frame belonging to the Ground Position
+
+    """
+    # init topocentric frame
+    if isinstance(gnd_pos, GeodeticPoint):
+        # ground position given as GeodeticPoint
+        if planet:
+            # planet is defined, use it
+            topo_frame = TopocentricFrame(planet, gnd_pos, "ground pos")
+        else:
+            # planet is not defined, use the default WGS84 Earth
+            itrf = FramesFactory.getITRF(IERSConventions.IERS_2010, True)
+            earth = OneAxisEllipsoid(
+                Constants.WGS84_EARTH_EQUATORIAL_RADIUS,
+                Constants.WGS84_EARTH_FLATTENING,
+                itrf,
+            )
+            topo_frame = TopocentricFrame(earth, gnd_pos, "ground pos")
+
+    else:
+        # ground position given directly as TopocentricFrame, just copy it
+        topo_frame = gnd_pos
+
+    return topo_frame
