8e20edfed74aeb1f91d894a24267e8a216b5791d
egemenimre <s.egemen.imre@gmail.com>
2023-03-29 00:15:31 +0200

    reshuffled the init satkit / orekit procedure

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,14 +4,12 @@
 #
 # Licensed under GNU GPL v3.0. See LICENSE.rst for more info.
 """
-Tests for satkit
+Tests for satkit.
 
 """
 from pathlib import Path
 
-from orekit.pyhelpers import setup_orekit_curdir
-
-from satkit import process_paths
+from satkit import init_satkit
 
 extra_path = Path(
     "..",
@@ -24,7 +22,4 @@ orekit_data_file_path = Path("data", "orekit-data", "orekit-data-reference.zip")
 def common_test_setup_module():
     """setup any state specific to the execution of the module."""
 
-    #  Init Orekit data
-    data_file = process_paths(extra_path, orekit_data_file_path).resolve()
-
-    setup_orekit_curdir(str(data_file))
+    init_satkit(orekit_data_file_path, extra_path)
