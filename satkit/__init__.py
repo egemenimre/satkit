d594a3e3357944fe7bff554eae23f172542056e8
egemenimre <s.egemen.imre@gmail.com>
2023-04-01 23:36:54 +0200

    added orekit data file download option

--- satkit/__init__.py
+++ satkit/__init__.py
@@ -11,6 +11,7 @@ __version__ = "0.0.1"
 from pathlib import Path
 
 import orekit
+import requests
 from orekit.pyhelpers import setup_orekit_curdir
 from pint import UnitRegistry
 
@@ -21,7 +22,7 @@ u = UnitRegistry()
 vm = orekit.initVM()
 
 
-def init_satkit(filepath: Path, *search_dirs: Path):
+def init_satkit(filepath: Path, *search_dirs: Path, download_data_file=False):
     """
     Inits Orekit using the orekit data file provided.
 
@@ -33,20 +34,30 @@ def init_satkit(filepath: Path, *search_dirs: Path):
     - Nominal path: `current working dir` + `filepath`
     - Alternate paths: `current working dir` + `alternate search dir` + `filepath`
 
+    If the `download_data_file` is set to `True`, the orekit data file zip will be
+    downloaded to the `filepath` provided. The default setting is `False`.
+
     Parameters
     ----------
     filepath
         File path
     search_dirs
         Alternate search directories
+    download_data_file
+        If `True`, attempts to download the orekit data file to the `filepath`
 
     Returns
     -------
     Path
         Path of the file, `None` if not found
     """
+    if download_data_file:
+        # download the orekit data file
+        download_orekit_data(filepath)
+
     #  Init Orekit data (add alternative path to look for the reference data)
     orekit_data_file_path = process_paths(filepath, *search_dirs)
+
     setup_orekit_curdir(str(orekit_data_file_path))
 
     return orekit_data_file_path
@@ -95,3 +106,16 @@ def process_paths(filepath: Path, *search_dirs: Path):
 
     # File is nowhere to be found, return None
     return None
+
+
+def download_orekit_data(dest_path) -> None:
+    """
+    Downloads the orekit data file to `dest_path`.
+    """
+    data_file_url = "https://gitlab.orekit.org/orekit/orekit-data/-/archive/master/orekit-data-master.zip"
+
+    with requests.get(data_file_url, stream=True, timeout=5) as r:
+        r.raise_for_status()
+        with open(dest_path, "wb") as f:
+            for chunk in r.iter_content(chunk_size=16 * 1024):
+                f.write(chunk)
