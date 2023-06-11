57145a77b8948702a3222a762db7e06e83b17c30
egemenimre <s.egemen.imre@gmail.com>
2023-06-11 23:46:10 +0200

    Added the tests for the new `GroundPassList` class

--- tests/events/test_eventfinders.py
+++ tests/events/test_eventfinders.py
@@ -7,10 +7,9 @@
 Tests for ground-based event and interval finders.
 
 """
-from math import radians
 
 from org.hipparchus.geometry.euclidean.threed import Vector3D
-from org.orekit.bodies import GeodeticPoint, OneAxisEllipsoid
+from org.orekit.bodies import OneAxisEllipsoid
 from org.orekit.frames import FramesFactory
 from org.orekit.orbits import KeplerianOrbit
 from org.orekit.propagation.analytical import KeplerianPropagator
@@ -21,11 +20,12 @@ from satkit import u
 from satkit.events.eventfinders import (
     StandardDawnDuskElevs,
     gnd_illum_finder,
-    gnd_pass_finder,
     sat_illum_finder,
 )
+from satkit.events.gnd_access import GroundPassList
 from satkit.time.time import AbsoluteDateExt
 from satkit.time.timeinterval import TimeInterval
+from satkit.utils.quantity_init import QuantityInit
 
 
 def earth():
@@ -54,10 +54,10 @@ def kep_propagator():
 
 
 def gnd_location():
-    longitude = radians(45.0)
-    latitude = radians(25.0)
-    altitude = 0
-    gnd_pos = GeodeticPoint(latitude, longitude, float(altitude))
+    longitude = 45.0 * u.deg
+    latitude = 25.0 * u.deg
+    altitude = 0 * u.m
+    gnd_pos = QuantityInit.geodetic_point(latitude, longitude, altitude)
 
     return gnd_pos
 
@@ -99,7 +99,19 @@ def elev_event_inputs_outputs():
         ),
     ]
 
-    input_output_tuples.append((nominal_search_interval, nominal_output))
+    nominal_max_elev_times = [
+        AbsoluteDateExt("2014-01-01T23:37:18.12814282699384Z", utc),
+        AbsoluteDateExt("2014-01-02T01:14:51.15712126350269Z", utc),
+        AbsoluteDateExt("2014-01-02T11:43:04.65476057497885Z", utc),
+        AbsoluteDateExt("2014-01-02T13:20:18.58229459858107Z", utc),
+        AbsoluteDateExt("2014-01-02T22:38:36.50265317272361", utc),
+    ]
+
+    nom_el_sample = 15.878761260282012 * u.deg
+
+    input_output_tuples.append(
+        (nominal_search_interval, nominal_max_elev_times, nom_el_sample, nominal_output)
+    )
 
     # Search starts inside a pass
     begin_with_pass_search_interval = TimeInterval(
@@ -112,8 +124,23 @@ def elev_event_inputs_outputs():
         AbsoluteDateExt("2014-01-01T23:42:49.64917646999244Z", utc),
     )
 
+    begin_with_pass_max_elev_times = [
+        AbsoluteDateExt("2014-01-01T23:37:18.12814281608175Z", utc),
+        AbsoluteDateExt("2014-01-02T01:14:51.15712126350266Z", utc),
+        AbsoluteDateExt("2014-01-02T11:43:04.65476057497933Z", utc),
+        AbsoluteDateExt("2014-01-02T13:20:18.58229459858155Z", utc),
+        AbsoluteDateExt("2014-01-02T22:38:36.50265317272363Z", utc),
+    ]
+
+    begin_with_pass_el_sample = 45.100729607576625 * u.deg
+
     input_output_tuples.append(
-        (begin_with_pass_search_interval, begin_with_pass_output)
+        (
+            begin_with_pass_search_interval,
+            begin_with_pass_max_elev_times,
+            begin_with_pass_el_sample,
+            begin_with_pass_output,
+        )
     )
 
     # Search ends inside a pass
@@ -127,20 +154,51 @@ def elev_event_inputs_outputs():
         AbsoluteDateExt("2014-01-02T22:38:00.000", utc),
     )
 
-    input_output_tuples.append((end_with_pass_search_interval, end_with_pass_output))
+    end_with_pass_max_elev_times = [
+        AbsoluteDateExt("2014-01-01T23:37:18.12814280241838Z", utc),
+        AbsoluteDateExt("2014-01-02T01:14:51.15712128164438Z", utc),
+        AbsoluteDateExt("2014-01-02T11:43:04.65476057498124Z", utc),
+        AbsoluteDateExt("2014-01-02T13:20:18.58229459857789Z", utc),
+        AbsoluteDateExt("2014-01-02T22:38:00.000Z", utc),
+    ]
+
+    end_with_pass_el_sample = 15.87876126028207 * u.deg
+
+    input_output_tuples.append(
+        (
+            end_with_pass_search_interval,
+            end_with_pass_max_elev_times,
+            end_with_pass_el_sample,
+            end_with_pass_output,
+        )
+    )
 
     # Search interval too short, but is inside a pass
     short_search_interval_1 = TimeInterval(
-        AbsoluteDateExt("2014-01-01T23:35:00.000", utc),
-        AbsoluteDateExt("2014-01-01T23:40:00.000", utc),
+        AbsoluteDateExt("2014-01-01T23:32:00.000", utc),
+        AbsoluteDateExt("2014-01-01T23:36:00.000", utc),
     )
     short_output_1 = [
         TimeInterval(
-            AbsoluteDateExt("2014-01-01T23:35:00.000", utc),
-            AbsoluteDateExt("2014-01-01T23:40:00.000", utc),
+            AbsoluteDateExt("2014-01-01T23:32:00.000", utc),
+            AbsoluteDateExt("2014-01-01T23:36:00.000", utc),
         )
     ]
-    input_output_tuples.append((short_search_interval_1, short_output_1))
+
+    short_search_max_elev_times_1 = [
+        AbsoluteDateExt("2014-01-01T23:36:00.000", utc),
+    ]
+
+    short_el_sample_1 = 17.100666187168592 * u.deg
+
+    input_output_tuples.append(
+        (
+            short_search_interval_1,
+            short_search_max_elev_times_1,
+            short_el_sample_1,
+            short_output_1,
+        )
+    )
 
     # Search interval is outside passes - no pass to be found
     no_pass_search_interval = TimeInterval(
@@ -148,7 +206,11 @@ def elev_event_inputs_outputs():
         AbsoluteDateExt("2014-01-02T21:40:00.000", utc),
     )
     no_pass_output = []
-    input_output_tuples.append((no_pass_search_interval, no_pass_output))
+    no_pass_max_elev_times = []
+
+    input_output_tuples.append(
+        (no_pass_search_interval, no_pass_max_elev_times, None, no_pass_output)
+    )
 
     return input_output_tuples
 
@@ -271,23 +333,44 @@ def deep_copy_intervals(interval_list):
 
 # we can't use pytest parametrize here, as elev_event_inputs_outputs() requires orekit to be initialised
 # parametrize is called before orekit init, causing an exception
-def test_elevation_events():
+def test_gnd_passes():
+    """Test for ground pass finding."""
     # elevation definition
     elevation = 5 * u.deg
 
     # loop through each input - output couple
-    for search_interval, expected_intervals in elev_event_inputs_outputs():
+    for (
+        search_interval,
+        expected_max_elev_times,
+        el_sample,
+        expected_intervals,
+    ) in elev_event_inputs_outputs():
         # find passes
-        passes = gnd_pass_finder(
-            search_interval,
-            gnd_location(),
-            elevation,
-            propagator=kep_propagator(),
-            planet=earth(),
+        gnd_pass_list = GroundPassList(
+            search_interval, gnd_location(), elevation, kep_propagator(), planet=earth()
         )
+
+        passes = gnd_pass_list.pass_intervals
+        max_elev_times = [
+            gnd_pass.max_elev_time for gnd_pass in gnd_pass_list.pass_list
+        ]
+
+        print("****************")
+        # print(max_elev_times)
+        if gnd_pass_list.pass_list:
+            print(gnd_pass_list.pass_list[0].az_el_list[2].el.to("deg"))
 
         # make sure output and expected value are of the same length
         assert len(passes.intervals) == len(expected_intervals)
+        assert len(max_elev_times) == len(expected_max_elev_times)
+        assert len(passes.intervals) == len(max_elev_times)
+
+        # check sample elevation values
+        if el_sample:
+            assert (
+                abs(gnd_pass_list.pass_list[0].az_el_list[2].el - el_sample)
+                < 1e-9 * u.deg
+            )
 
         # check each interval to ensure equality
         for interval, exp_interval in zip(passes.intervals, expected_intervals):
@@ -298,6 +381,18 @@ def test_elevation_events():
             # print("*********")
 
             assert interval.is_equal(exp_interval, tolerance=100 * u.ns)
+
+        # check each max elevation time to ensure equality
+        for max_elev_time, exp_max_elev_time in zip(
+            max_elev_times, expected_max_elev_times
+        ):
+            # if not interval.is_equal(exp_interval):
+            # print(interval)
+            # print("--------")
+            # print(exp_interval)
+            # print("*********")
+
+            assert max_elev_time.isCloseTo(exp_max_elev_time, tolerance=100 * u.ns)
 
 
 # we can't use pytest parametrize here, as elev_event_inputs_outputs() requires orekit to be initialised
